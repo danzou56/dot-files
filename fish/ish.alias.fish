4cc7ffa6150bfe6f1751c2252bec075bb8a0b6ed
Dan Zou <danny81299@gmail.com>
2021-03-12 23:44:50 -0500

    ls if cat on directory

--- fish/ish.alias.fish
+++ fish/ish.alias.fish
@@ -1,4 +1,15 @@
 alias ls="exa -h -F --group-directories-first --icons --color=always"
-alias cat="batcat"
 alias wopen="wslview"
 
+function cat
+  if test (count $argv) -gt 1
+    batcat $argv
+  else
+    if test -d $argv[1]
+      echo "`$argv[1]` was a directory! Doing `ls $argv` instead"
+      ls $argv
+    else
+      batcat $argv
+    end
+  end
+end
